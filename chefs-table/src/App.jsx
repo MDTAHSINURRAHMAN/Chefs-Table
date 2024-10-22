190d4078f7de4efdbe6e1effcab1703910afbb2c
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-10-22 19:00:31 +0600

    Recipe Cards Section

--- chefs-table/src/App.jsx
+++ chefs-table/src/App.jsx
@@ -1,16 +1,19 @@
-import Banner from "./components/Banner";
-import Header from "./components/Header";
-import OurRecipes from "./components/OurRecipes";
+import Recipes from "./components/Recipes";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <div className="container mx-auto px-4">
       {/* Header Section */}
-      <Header></Header>
       {/* Banner Section */}
-      <Banner></Banner>
       {/* Our Recipes Section */}
-      <OurRecipes></OurRecipes>
+      {/* Recipe Card Section */}
+      <section className="flex flex-col md:flex-row gap-6">
+        {/* Cards Section */}
+        <Recipes></Recipes>
+        {/* Sidebar */}
+        <Sidebar></Sidebar>
+      </section>
     </div>
   );
 };
